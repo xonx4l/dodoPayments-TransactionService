ae506981b95ddc7d313f3f4e35c34c7d2dca4944
xonx4l <sk797119@gmail.com>
2025-09-16 17:27:38 +0530

    add database initilization

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
 mod models;
+mod database;
 
 use axum::{http::Method, middleware, routing::{get, post, delete}, Router};
+
+use crate::{
+    database::Database;
+}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
 
     init_tracing()?;
+
+    let database = Database::new().await?;
+
+    let database = Arc::new(database);
 }
