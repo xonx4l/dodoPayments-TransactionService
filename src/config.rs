932bd5db8468dc3bcf0a38a9a3ebb219d2660cc3
xonx4l <sk797119@gmail.com>
2025-09-17 07:22:13 +0530

    update ports and database url

--- src/config.rs
+++ src/config.rs
@@ -10,5 +10,16 @@ pub struct Config {
 impl config {
     pub fn load() -> Result<Self> {
         dotenvy::dotenv().ok();
+
+        let config = Config {
+            port: env::var("PORT")
+               .unwrap_or_else(|_| "3000".to_string())
+               .parse()
+               .unwrap_or(3000),
+            database_url: env::var("DATABASE_URL")
+               .unwrap_or_else(|_| "postgres://postgres:password@localhost:5432/transaction_service".to_string()),
+        };
+
+        Ok(config)
     }
 }
